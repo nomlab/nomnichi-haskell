5201a51824044f7b0d5fdc61f137d404353ec7ba
fukai <fukai@kawasemi2.swlab.cs.okayama-u.ac.jp>
2012-10-19 13:11:10 +0900

    ログインに失敗し，再度ログインフォームを表示するとき，IDフォームの中身を残す．

--- Handler/Loginform.hs
+++ Handler/Loginform.hs
@@ -3,23 +3,14 @@ module Handler.Loginform
     , postLoginformR
     )
 where
-
 import Import
 import Data.Monoid()
-
--- to use Html into forms
--- import Yesod.Form.Nic (YesodNic, nicHtmlField)
--- instance YesodNic App
-
-loginForm :: Form Loginuser
-loginForm = renderDivs $ Loginuser
-    <$> areq   textField "UserId" Nothing
-    <*> aopt   textField "Password" Nothing
 
 getLoginformR :: Handler RepHtml
 getLoginformR = do
     deleteSession "loginname"
-    defaultLayout $ do 
+    id <- return ("" :: Text)
+    defaultLayout $ do
         $(widgetFile "loginform")
         
 postLoginformR :: Handler RepHtml
@@ -28,34 +19,15 @@ postLoginformR = do
      loginname <- runInputPost $ ireq textField "UserId"
      passwd <- runInputPost $ iopt textField "Password"
      user <- runDB $ selectList [(LoginuserIdstr ==. loginname), (LoginuserPassword ==. passwd)] [Desc LoginuserId]
-
      case user of
            [] ->do 
-            setMessage "login ERROR!" 
-            redirect $ LoginformR
-
-           --    redirectUltDest HomeR
+            setMessage "login ERROR!"
+            id <- return loginname --再度ログインページを表示する際に，IDフォームの中身を残す
+            defaultLayout $ do
+                   $(widgetFile "loginform")
            _ -> do       -- 一致した場合
               -- POST された名前を取得し、セッションに設定する
               loginname <- runInputPost $ ireq textField "UserId"
               setSession "loginname" loginname
               -- その後、最終転送先orデフォルトへリダイレクトする
               redirectUltDest HomeR
-
---    ((res,loginuserWidget), enctype) <- runFormPost makingForm
---    case res of
---         FormSuccess loginuser -> do 
---            loginuserId <- runDB $ insert loginuser 
---            setMessage $ toHtml $ (loginuserIdstr loginuser) <> " created" 
---            redirect $ UserR
---         _ -> defaultLayout $ do
---                setTitle "Please correct your entry form"
---                $(widgetFile "userAddError")
-
-
--- postNewuserR :: ArticleId -> Handler RepHtml
--- postNewuserR articleId = do
---    article <- runDB $ get404 articleId
---    defaultLayout $ do
---        setTitle $ toHtml $ articleTitle article
---        $(widgetFile "article")
