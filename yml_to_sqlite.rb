c7893f1b22db634799f687c1dc1b70fe339689cd
Hideto Yoshii <blue.snow.sea.world@gmail.com>
2013-05-28 13:44:58 +0900

    Modify a script

--- yml_to_sqlite.rb
+++ yml_to_sqlite.rb
@@ -9,6 +9,9 @@ Ruby1.9.3でのみ動作確認した．
 このプログラムでは，テーブルの作成は行わない．
 このため，以下の手順は，ノムニチを一度起動し，テーブルが作成済みであることが前提となる．
 
+ノムニチの記事のpermalinkにより，登録済みかどうかを判定する．
+登録されていない記事のみを取り込む．
+
 -- 使用方法
 nomnichi.sqlite3 と同じ階層に，yml_to_sqlite.rb と nomnichi.yaml を置く．
 以下のコマンドを実行することで，nomnichi.sqlite3 へデータが登録される．
@@ -24,23 +27,26 @@ require 'yaml'
 
 def into_article_table(yaml_data)
   last_id = serach_biggest_id("article")
+  perma_link_list = get_perma_link_list("article")
   number_of_article = 0
   @nomnichi_db.transaction do
     yaml_data.each do |d|
-      last_id += 1
-      number_of_article += 1
-      article_sql = <<SQL
+      unless existing_perma_link?(d["perma_link"], perma_link_list)
+        last_id += 1
+        number_of_article += 1
+        article_sql = <<SQL
 insert into article values ( #{last_id.to_s}, "#{d["member_name"]}", "#{replace_escape_character(d["title"])}", "#{d["perma_link"]}", "#{d["content"]}", "#{d["created_on"]}", "#{d["updated_on"]}", "#{d["published_on"]}", #{d["approved"].to_i}, #{d["count"].to_i}, #{d["promote_headline"].to_i});
 SQL
-      @nomnichi_db.execute(article_sql)
-      print '.'
-      d["comment"].each do |c|
-        comment_sql = <<SQL
+        @nomnichi_db.execute(article_sql)
+        print '.'
+        d["comment"].each do |c|
+          comment_sql = <<SQL
 insert into comment(commenter, body, created_at, updated_at, article_id) values ( "#{c["commenter"]}", "#{c["body"]}", "#{c["created_at"]}", "#{c["updated_at"]}", #{last_id});
 SQL
-        @nomnichi_db.execute(comment_sql)
-# insert into comment(commenter, body, created_at, update_at, article_id) values ( "murata", "コメント投稿できましたか？", "2013-03-17 20:50:02 +0900", "2013-03-17 20:50:02 +0900", 5);
-      end if d["comment"]
+          @nomnichi_db.execute(comment_sql)
+          # insert into comment(commenter, body, created_at, update_at, article_id) values ( "murata", "コメント投稿できましたか？", "2013-03-17 20:50:02 +0900", "2013-03-17 20:50:02 +0900", 5);
+        end if d["comment"]
+      end
     end
   end
   print "\nProbably registration has been succeeded. (" +
@@ -58,6 +64,24 @@ def serach_biggest_id(table_name)
     max = row[0] if row[0] > max
   end
   return max
+end
+
+def get_perma_link_list(table_name)
+  perma_link_list = []
+  sql = "SELECT * FROM " + table_name + ";"
+  @nomnichi_db.execute(sql) do |row|
+    perma_link_list << row[3]
+  end
+  return perma_link_list
+end
+
+def existing_perma_link?(perma_link, perma_link_list)
+  perma_link_list.each do |p|
+    if perma_link == p
+      return true
+    end
+  end
+  return false
 end
 
 @nomnichi_db = SQLite3::Database.new("nomnichi.sqlite3")
