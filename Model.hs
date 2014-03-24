9132dcdae924aa3a784774589bd7a80d56598e14
Kitagaki Chihiro <kitagaki@swlab.cs.okayama-u.ac.jp>
2014-03-24 20:04:42 +0900

    Change authetication from basic to HashDB

--- Model.hs
+++ Model.hs
@@ -6,6 +6,7 @@ import Data.Text (Text)
 import Database.Persist.Quasi
 import Data.Typeable (Typeable)
 import Data.Time
+import Yesod.Auth.HashDB (HashDBUser(..))
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
@@ -13,3 +14,10 @@ import Data.Time
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlOnlySettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
+instance HashDBUser User where
+    userPasswordHash = userPassword
+    userPasswordSalt = Just . userSalt
+    setSaltAndPasswordHash s h p = p { userSalt = s,
+                           userPassword = Just . h
+                           }
