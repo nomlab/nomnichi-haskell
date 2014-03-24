d5f0ee60106d2a281651b0d608d08d7f17aea592
Kitagaki Chihiro <kitagaki@swlab.cs.okayama-u.ac.jp>
2014-03-24 21:24:23 +0900

    Fix bug in script to add user to sqlite3

--- add_user_to_sqlite.rb
+++ add_user_to_sqlite.rb
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 require 'sqlite3'
-
-@database_yml = ARGV[0]
+require 'digest/sha1'
 
 @nomnichi_db = SQLite3::Database.new("nomnichi.sqlite3")
+
 print "User : "; user = gets.chomp
 print "Pass : "; pass = gets.chomp
-salt = 'sio'#Time.now.to_s
-puts salt
-pass_sha = `echo -n '#{salt}#{pass}' | shasum`.chomp.split(' ').first
 
-puts pass_sha
+salt = Time.now.to_s
+
+pass_sha = Digest::SHA1.hexdigest(salt + pass)
+
 max_id_sql = <<SQL
 select max(id) from user;
 SQL
@@ -22,8 +22,5 @@ insert into user values (#{id}, '#{user}', '#{pass_sha}', '#{salt}');
 SQL
 @nomnichi_db.execute(add_user_sql)
 
-
-# yaml_data = YAML.load_file("./#{@database_yml}")
-# into_article_table(yaml_data)
 @nomnichi_db.close
 
