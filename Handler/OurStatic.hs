7955f2888d16f16a68e7b11b068031e260245595
Yuya Murata <murata1224@gmail.com>
2014-03-31 18:07:35 +0900

    Make routing handle jpg and png

--- Handler/OurStatic.hs
+++ Handler/OurStatic.hs
@@ -13,16 +13,25 @@ import System.IO (readFile)
 import Text.Blaze.Html (preEscapedToHtml)
 import Data.ByteString.Char8 (unpack)
 import Yesod.Form.Nic (YesodNic, nicHtmlField)
+
 instance YesodNic App
 
-readStaticFile :: FilePath -> Html
-readStaticFile filePath = preEscapedToHtml $ unsafePerformIO $ readFile filePath
+readStaticFile :: FilePath -> String
+readStaticFile filePath = unsafePerformIO $ readFile filePath
 
 textsToFilePath :: [Text] -> FilePath
 textsToFilePath texts = "public_html/" ++ ( init $ tail $ show $ DT.intercalate "/" texts )
 
 getOurStaticR :: [Text] -> Handler Html
-getOurStaticR path = do
-    defaultLayout $ do
-      [whamlet|#{readStaticFile $ textsToFilePath path}|]
+getOurStaticR path = 
+    case fileExtension of
+-- 今後，以下の方法に書き換える
+-- http://hackage.haskell.org/package/mime-types-0.1.0.0/docs/Network-Mime.html#t:FileName
+      "html" -> defaultLayout $ do
+                  [whamlet|#{preEscapedToHtml body}|]
+      "png" -> sendFile typePng $ textsToFilePath path
+      "jpg" -> sendFile typeJpeg $ textsToFilePath path
+    where 
+      body = readStaticFile $ textsToFilePath path
+      fileExtension = reverse $ fst $ break (=='.') $ reverse $ textsToFilePath path
 
