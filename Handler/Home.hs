3dc5ecb45a81c93cd9d58df8e20c4d7b57879ec1
yuko <ike534xx@yahoo.co.jp>
2014-09-30 11:44:20 +0900

    Display folded articles on top_page

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,6 +3,9 @@ module Handler.Home where
 
 import Import as I
 import Data.Time
+import Data.List as I (lines, unlines, isPrefixOf)
+import Text.Blaze.Html (preEscapedToHtml)
+import Text.Blaze.Html.Renderer.String (renderHtml)
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -20,3 +23,42 @@ getHomeR = do
         aDomId <- newIdent
         setTitle "乃村研究室ホームページ"
         $(widgetFile "homepage")
+
+
+-- local functions --
+takeHeadLine :: Html -> Html
+takeHeadLine content = preEscapedToHtml $ prettyHeadLine $ renderHtml content
+
+prettyHeadLine :: String -> String
+prettyHeadLine article = gsub "_br_" "<br>" $ stripTags $ gsub "<br>" "_br_" $ I.unlines $ foldArticle $ I.lines article
+
+stripTags' bool (x:xs)
+  | xs   == []    = if x == '>'
+                    then []
+                    else [x]
+  | bool == True  = if x == '>'
+                    then stripTags' False xs
+                    else stripTags' True xs
+  | bool == False = if x == '<'
+                    then stripTags' True xs
+                    else x : (stripTags' False xs)
+stripTags str = stripTags' False str
+
+gsub _ _ [] = []
+gsub x y str@(s:ss)
+  | I.isPrefixOf x str = y ++ gsub x y (drop (length x) str)
+  | otherwise = s:gsub x y ss
+
+foldArticle :: [String] -> [String]
+foldArticle lines = if lines == headLine
+                    then take 3 lines
+                    else headLine
+  where headLine = foldAtFolding lines
+
+foldAtFolding :: [String] -> [String]
+foldAtFolding (x:xs)
+  | x /= "<!-- folding -->" = if xs == []
+                              then [x]
+                              else x:foldAtFolding xs
+  | x == "<!-- folding -->" = []
+  | otherwise = []
